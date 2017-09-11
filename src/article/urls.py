c5c19dc0c0887f594d4697e8603409724b66109d
fauziame <fauzi@eliquidscanada.com>
2017-09-10 22:40:52 -0400

    add src

---
+++ src/article/urls.py
@@ -0,0 +1,17 @@
+
+from django.conf.urls import url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.article_list, name='list'),
+    url(r'^article/$', views.article_list2, name='article'),
+
+    #url(r'^/category/(?P<slug>[\w-]+)/$', views.cat_list, name='cat'),
+    url(r'^create/', views.article_create, name='create'),
+    url(r'^(?P<slug>[\w-]+)/$', views.article_detail, name='detail'),
+    url(r'^(?P<slug>[\w-]+)/edit', views.article_update, name='edit'),
+    url(r'^(?P<slug>[\w-]+)/delete/$', views.article_delete, ),
+    url(r'^category/(?P<slug>[\w-]+)/$', views.view_category, name='cat'),
+
+]
